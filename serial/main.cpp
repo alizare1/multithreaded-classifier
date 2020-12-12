46250aa01184e9d07caa19343c379f93a97e92d0
alizare1 <ali.zare2000123@gmail.com>
2020-12-12 16:07:30 +0330

    complete serial

--- serial/main.cpp
+++ serial/main.cpp
@@ -8,11 +8,12 @@
 #include <sstream> 
 #include <iterator>
 #include <algorithm>
+#include <limits>
 
 using namespace std;
 
 
-void read_train(vector<vector<float>> &train, ifstream &file) {
+void read_csv(vector<vector<float>> &vec, ifstream &file) {
     
     string line, tmp;
     int i;
@@ -25,7 +26,7 @@ void read_train(vector<vector<float>> &train, ifstream &file) {
         i = 0;
         while (getline(ss, tmp, ',')) {
             value = stof(tmp);
-            train[i].push_back(value);
+            vec[i].push_back(value);
             i++;
         }
     }
@@ -52,6 +53,39 @@ void normalize(vector<vector<float>> &train) {
     }
 }
 
+void classify(
+    const vector<vector<float>> &train,
+    const vector<vector<float>> &weights, 
+    vector<int> &results
+    ) {
+
+    for (uint i = 0; i < train[0].size(); i++) {
+        float scores[4] = {
+            weights[20][0], 
+            weights[20][1], 
+            weights[20][2], 
+            weights[20][3]
+        };
+        for (uint j = 0; j < train.size() - 1; j++) {
+            scores[0] += train[j][i] * weights[j][0];
+            scores[1] += train[j][i] * weights[j][1];
+            scores[2] += train[j][i] * weights[j][2];
+            scores[3] += train[j][i] * weights[j][3];
+        }
+
+        results.push_back(max_element(scores, scores + 4) - scores);
+    }
+}
+
+float get_accuracy(const vector<float> &answer, const vector<int> &predict) {
+    int count = 0;
+    for (uint i = 0; i < answer.size(); i++)
+        if (answer[i] == predict[i])
+            count++;
+
+    return (float) count / (float) answer.size();
+}
+
 int main(int argc, char *argv[]) {
     ifstream train, weights;
 
@@ -63,10 +97,16 @@ int main(int argc, char *argv[]) {
     train.open(string(argv[1]) + "/train.csv");
     weights.open(string(argv[1]) + "/weights.csv");
 
-    vector<vector<float>> train_data(21);
-    read_train(train_data, train);
-    
+    vector<vector<float>> train_data(21), weights_data(21);
+    vector<int> results;
+    read_csv(train_data, train);
+    read_csv(weights_data, weights);
     normalize(train_data);
+    classify(train_data, weights_data, results);
+    
+    printf("%.2f\n", get_accuracy(train_data[20], results) * 100);
+
+
 
     exit(EXIT_SUCCESS);
 }
